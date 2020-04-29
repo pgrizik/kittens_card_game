52a3d7bc5989d162fd3f87a548144e9e948c362a
pgrizik <pgrudge@gmail.com>
2020-04-29 11:41:04 +0300

    Updates

---
+++ precache-manifest.d1b6fedea62cbe1dce602d3cc36b30a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a51c8d65d86f50fb04fc48911f64a1",
+    "url": "/kittens_card_game/index.html"
+  },
+  {
+    "revision": "e1ee9cc1dba59d672935",
+    "url": "/kittens_card_game/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "601944b222594cda9e5d",
+    "url": "/kittens_card_game/static/css/main.94aaee58.chunk.css"
+  },
+  {
+    "revision": "e1ee9cc1dba59d672935",
+    "url": "/kittens_card_game/static/js/2.0c1e4e58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittens_card_game/static/js/2.0c1e4e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601944b222594cda9e5d",
+    "url": "/kittens_card_game/static/js/main.b0c828ae.chunk.js"
+  },
+  {
+    "revision": "1e58c5dcc7b3c91836d6",
+    "url": "/kittens_card_game/static/js/runtime-main.c26afcec.js"
+  }
+]);
